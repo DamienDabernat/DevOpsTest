317b066feb58b358913eb90eebd02c1229f79006
Damien Dabernat <damien.dabernat@gmail.com>
2018-10-11 11:41:49 +0200

    test

--- app/__tests__/app.test.js
+++ app/__tests__/app.test.js
@@ -12,6 +12,7 @@ describe('index route', () => {
     return request(app)
       .get('/')
       .expect('Content-Type', /html/)
+      .expect(200)
       .then(response => {
         expect(response.text).toMatch(
           /<title>Express App Testing Demo<\/title>/
