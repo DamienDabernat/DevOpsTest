a62e7a01335d77c43adace3a0f899361dbeca32a
Damien <damien.dabernat@gmail.com>
2018-10-12 17:47:02 +0200

    resolve #1

--- app/server.js
+++ app/server.js
@@ -11,6 +11,7 @@ const DDMiddleware = require('connect-datadog');
 // Datadog middleware for Connect JS / Express
 app.use(DDMiddleware({dogstatsd: ddagent}));
 
+//test
 
 // public assets
 app.use(express.static(path.join(__dirname, 'public')));
